830d5a7f7371989f8b60fa18c03efd2ea513101e
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-04-26 17:54:45 +0100

    Initial Commit

---
+++ Hangman/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hangman
+//
+//  Created by Jakub Gawecki on 26/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
